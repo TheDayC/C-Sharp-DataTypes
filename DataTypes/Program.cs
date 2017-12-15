4657b05024cf03a541528f0da22c2ed24bb9a5d5
David Connolly <daveconnolly90@gmail.com>
2017-12-15 13:28:42 +0000

    Functionality, string builder, interpolation

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -11,6 +11,12 @@ namespace DataTypes {
 			LocalVarDeclarations();
 			ParseFromStrings();
 			UseDatesAndTimes();
+			StringFunctionality();
+			StringBuilderExamples();
+			StringInterpolation();
+
+			// Cleanup
+			Console.WriteLine();
 			Console.ReadLine();
 		}
 
@@ -31,8 +37,6 @@ namespace DataTypes {
 			System.Boolean b4 = false;
 
 			Console.WriteLine("Your data: {0}, {1}, {2}, {3}, {4}, {5}", myInt, myString, b1, b2, b3, b4);
-
-			Console.WriteLine();
 		}
 
 		static void ParseFromStrings() {
@@ -72,6 +76,67 @@ namespace DataTypes {
 
 			// Subtract 15 mins from current TimeSpan
 			Console.WriteLine(ts.Subtract(new TimeSpan(0, 15, 0)));
+		}
+
+		static void StringFunctionality() {
+			Console.WriteLine("=> Basic String functionality:");
+			string firstName = "David", lastName = "C";
+			
+			// Base value
+			Console.WriteLine("Value of firstName: {0}", firstName);
+
+			// Character length
+			Console.WriteLine("firstName has {0} characters", firstName.Length);
+
+			// Case transformation
+			Console.WriteLine("Uppercase: {0}", firstName.ToUpper());
+			Console.WriteLine("Lowercase: {0}", firstName.ToLower());
+
+			// String search
+			Console.WriteLine("Contains the string y?: {0}", firstName.Contains("y"));
+
+			// Replace
+			Console.WriteLine("Shorten name with string replace. {0} to {1}", firstName, firstName.Replace("id", "e"));
+
+			// Concatenation
+			Console.WriteLine("First and Last names: {0}", firstName + " " + lastName);
+
+			// Escapes
+			Console.WriteLine("{0}\'s \"string\" with \n various \t escapes", firstName);
+
+			// Verbatim string - useful for declaring file paths / anything using escape characters and for preserving whitespace / multi line strings.
+			Console.WriteLine(@"String with escape chars: C:\MyApp\bin\Debug");
+
+			// Comparison
+			Console.WriteLine("firstName == lastName: {0}", firstName == lastName);
+		}
+
+		static void StringBuilderExamples() {
+			// Games array
+			string[] goodGames = new string[] { "Half Life 2", "Morrowind", "Deus Ex", "System Shock" };
+
+			Console.WriteLine("=> Using the StringBuilder:");
+
+			// Title
+			StringBuilder sb = new StringBuilder("**** Fantastic Games ****");
+			sb.Append("\n");
+
+			// Add games to string builder
+			foreach (string game in goodGames) {
+				sb.AppendLine(game);
+			}
+
+			// Output stringbuilder string
+			Console.WriteLine(sb.ToString());
+		}
+
+		static void StringInterpolation() {
+			int age = 27;
+			string name = "Liliana";
+
+			// String interpolation
+			string greeting = $"Welcome {name}. You\'re {age} years old.";
+			Console.WriteLine(greeting);
 		}
 	}
 }
